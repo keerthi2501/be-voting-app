1b44df7fef35f02987a601f06f834e41aa2f677e
M Keerthi Reddy <m-keerthi.reddy@t-systems.com>
2025-05-13 16:45:58 +0530

    Changes to set the Vote count to each option

--- votingapp/src/main/java/com/Voting/votingapp/services/PollService.java
+++ votingapp/src/main/java/com/Voting/votingapp/services/PollService.java
@@ -1,8 +1,10 @@
 package com.Voting.votingapp.services;
 
+import com.Voting.votingapp.model.OptionVote;
 import com.Voting.votingapp.model.Poll;
 import com.Voting.votingapp.repositories.PollRepository;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,4 +24,29 @@ public class PollService {
     return pollRepository.findAll();
   }
 
-}
+  public Optional<Poll> getPollById(Long id){
+    return pollRepository.findById(id);
+  }
+
+  public void vote(Long pollId, int optionIndex){
+    //Get Poll from DB
+    Poll poll = pollRepository.findById(pollId).orElseThrow(()-> new RuntimeException("Poll not found"));
+
+    //Get all Options
+    List<OptionVote> options = poll.getOptions();
+
+    //If index for vote is not valid, throw error
+    if(optionIndex<0 || optionIndex >= options.size()){
+      throw  new IllegalArgumentException("Invalid option index");
+    }
+      //Get Selected Option
+      OptionVote selectedOption = options.get(optionIndex);
+
+      //Increment vote for selected option
+      selectedOption.setVoteCount(selectedOption.getVoteCount() + 1);
+
+      //Save incremented option index
+      pollRepository.save(poll);
+    }
+
+  }
